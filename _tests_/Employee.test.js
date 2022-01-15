8c0f85f735ad13eb833df9f5d0f60e775aa10ddc
andrew wales <andrew.derek.wales@gmail.com>
2022-01-14 18:38:16 -0600

    adding commit to push to my desktop

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,14 @@
 const Employee = require('../lib/Employee')
 
 // test for the getName() method
-test('creates an employee object', () => {
-    const employee = new Employee('Jim')
+test('creates an employee name', () => {
+    
+    const name = 'jim';
+    const y = new Employee(name);
 
-    expect(Employee.name).toBe('Jim')
+    expect(getName).toEqual(name);
+});
+
+test('creates an id for the employee', () => {
+    expect(Employee.getId()).tobe(id);
 });
